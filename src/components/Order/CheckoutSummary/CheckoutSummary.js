fed9abf3e015fb7ad781fd606a4e689deb066efc
Muhammed Fatih ÖZDİL <muhammedfatihozdil@gmail.com>
2021-02-13 11:24:10 +0300

    add navigating back and to  to next page for checkout page

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -11,11 +11,11 @@ const CheckoutSummary = props => {
             </div>
             <Button
                 btnType="Danger"
-                clicked
+                clicked={props.checkoutCancelled}
             >CANCEL</Button>
             <Button
                 btnType="Success"
-                clicked
+                clicked={props.checkoutContinued}
             >CONTINUE</Button>
         </div>
     )
